40f53fffa1566f1cebeac84eb76afc407f5e2f4c
chilya02 <ilya.cherepanov.02@mail.ru>
2023-04-25 18:56:10 +0300

    Адаптив всплывающих окон под мобильные устройства

--- api/services.py
+++ api/services.py
@@ -43,10 +43,7 @@ def get_buy_list_html(section: str, user: User) -> str:
         template_name='p2p/posts_table.html', 
         context={'section': 'buy', 'posts': posts, 'user': user}
     )
-    return render_to_string(
-        template_name='p2p/posts.html',
-        context={'section': 'buy', 'posts': posts, 'user': user}
-    )
+    return result_html
 
 
 def get_user_posts_list_html(user: User, section: str) -> str:
