3c4eb67fdd91b1ac73181de9becd4149dac26665
chilya02 <ilya.cherepanov.02@mail.ru>
2023-04-26 18:09:24 +0300

    Создан ORM для nft

--- crypto/settings.py
+++ crypto/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'api',
     'p2p',
     'authentication',
+    'nft',
 ]
 
 MIDDLEWARE = [
--- nft/models.py
+++ nft/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+from django.db import models
+from authentication.models import User
+
+
+class Collection(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=20)
+    changeable = models.BooleanField(verbose_name='Изменямость', default=True)
+
+
+class NftImage(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=20)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец')
+    price = models.IntegerField(verbose_name='Цена')
+    collection = models.ForeignKey(
+        Collection,
+        on_delete=models.CASCADE,
+        verbose_name='Коллекция',
+        related_name='images'
+    )
+
