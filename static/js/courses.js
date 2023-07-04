e3a2b4d59a353a35007fc9434e508a7cbdd97a6e
chilya02 <ilya.cherepanov.02@mail.ru>
2023-07-04 21:52:19 +0300

    front listeners added

--- static/js/courses.js
+++ static/js/courses.js
@@ -1,1 +1,8 @@
-function LoadCourses()
+function connectCourses(){
+    const socket = new WebSocket(`ws:${window.location.host}/ws/courses/get-courses`);
+    socket.onclose = connectCourses;
+    socket.onmessage = function(e){updateCourses(e.data)}
+}
+function updateCourses(data){
+    console.log(data);
+}
