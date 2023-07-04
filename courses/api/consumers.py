fae45cd26576ec33534d94b758314222b6d4a875
chilya02 <ilya.cherepanov.02@mail.ru>
2023-07-04 23:45:26 +0300

    consumer refactored

--- courses/api/consumers.py
+++ courses/api/consumers.py
@@ -4,7 +4,6 @@ from courses.models import get_courses
 import json
 
 
-
 class CoursesConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,16 +14,12 @@ class CoursesConsumer(AsyncWebsocketConsumer):
         await self.send('123')
         self.sending_task = await asyncio.ensure_future(self.run_periodic_task())
 
-
     async def run_periodic_task(self):
         while True:
             data = await get_courses()
             await self.send(json.dumps(data))
             await asyncio.sleep(1)
 
-    def receive(self, text_data=None, bytes_data=None):
-        print('recived:', text_data)
-
-    def disconnect(self, code):
+    async def disconnect(self, code):
         print('disconnected')
         self.sending_task.cancel()
